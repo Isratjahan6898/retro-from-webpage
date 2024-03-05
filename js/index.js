6177b95d4dd0fac50efeeca27655a6922b823c09
israt6898 <israt6898@gmail.com>
2024-03-05 22:45:42 +0600

    change something

--- js/index.js
+++ js/index.js
@@ -1,26 +1,38 @@
-const loadCard = async() =>{
-    const res = await fetch(' https://openapi.programming-hero.com/api/retro-forum/posts');
-    const data = await res.json();
-    const cards= data.posts;
-    displayCard(cards);
-    displayTitle(cards);
+let titleCount = 0;
+
+const loadCard = async () => {
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'block';
+    try {
+        const res = await fetch(`  https://openapi.programming-hero.com/api/retro-forum/posts`);
+        const data = await res.json();
+        const cards = data.posts;
+        displayCard(cards);
+        console.log(cards);
+    }
+    finally {
+        spinner.style.display = 'none';
+    }
 }
 
-
-const displayCard = cards =>{
+const displayCard = cards => {
+    const cardContainer = document.getElementById('card-container');
+    cardContainer.innerHTML = "";
 
     cards.forEach(card => {
-
-        //console.log(card);
-         
-        const cardContainer = document.getElementById('card-container');
-
         const showCard = document.createElement('div');
-        showCard.classList=`card card-side mb-[16px] bg-[#F3F3F5] shadow-xl`;
-        showCard.innerHTML=`<figure><div class="indicator">
-        <span class="indicator-item badge badge-secondary mt-[30px] mr-[10px] ${card?.isActive===true? ' green':' red'}"></span> 
-        <div class="grid w-[72px] h-[72px] mt-[30px] ml-[30px] bg-base-300 place-items-center ">  <img src="${card.image}" alt=""></div>
-      </div></figure>
+        showCard.classList = `card card-side mb-[16px] bg-[#F3F3F5] shadow-xl`;
+        const indicatorStyle = card.isActive ? 'background-color: #00cc00' : ''
+        showCard.innerHTML = `
+        <figure>
+            <div class="indicator">
+                    <span class="indicator-item badge badge-secondary mt-[30px] mr-[10px]" style="${indicatorStyle}"></span>
+                <div class="grid w-[72px] h-[72px] mt-[30px] ml-[30px] bg-base-300 place-items-center ">  
+                    <img src="${card.image}" alt="">
+                </div>
+            </div>
+        </figure>
+
       <div class="card-body">
 
         <div class="flex flex-row mb-[16px] ">
@@ -28,14 +40,12 @@ const displayCard = cards =>{
           <h1>Author:${card.author.name}</h1>
         </div>
 
+        <h2 class="card-title mb-[16px]"> ${card.title}</h2>
+        <p class="mb-[16px] text-[#12132D99]">${card.description}</p>
 
-        <h2 class="card-title mb-[16px]"> ${card.title}
-        </h2>
-        <p class="mb-[16px] text-[#12132D99]">${card.description}
-        </p>
         <hr>
-        <div class="card-actions justify-between">
 
+        <div class="card-actions justify-between">
           <div class="flex">
             <p class="mr-[10px]"><i class="fa-regular fa-message"></i></p>
             <h1>${card.comment_count}</h1>
@@ -50,25 +60,55 @@ const displayCard = cards =>{
             <p class="mr-[10px]"><i class="fa-regular fa-clock"></i></p>
             <h1>${card.posted_time} Min</h1>
           </div>
-          
 
-          <button><i class="fa-regular fa-envelope text-center w-[40px] h-[40px] rounded-[50px] p-[8px] bg-[#10B981]" aria-hidden="true"></i></button>
+          <button onclick="displayCardTitle('${card.title}')"><i class="fa-regular fa-envelope text-center w-[40px] h-[40px] rounded-[50px] p-[8px] bg-[#10B981]" aria-hidden="true"></i></button>
+
         </div>
-      
       </div>
     </div>`
-
-
-     cardContainer.appendChild(showCard);
+        cardContainer.appendChild(showCard);
     });
 
 }
 
+const displayCardTitle = (title) => {
+    const sidebar = document.getElementById('sidebar');
+    const titleCountElement = document.getElementById('titleCount');
 
-const displayTitle = cards =>{
-    console.log(cards)
-    
+    const titleElement = document.createElement('div');
+    titleCount++;
+    titleElement.innerHTML = ` ${title}`;
+    sidebar.appendChild(titleElement);
+    titleCountElement.innerHTML = `Mark as read: ${titleCount}`;
 }
+
+const handleSearch = async () => {
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'block';
+
+    try {
+        const categorySearch = document.getElementById('categorySearch').value.toLowerCase();
+        const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
+        const data = await res.json();
+        const cards = data.posts;
+
+        const filteredCards = cards.filter(card => card.category.toLowerCase().includes(categorySearch));
+
+        displayCard(filteredCards);
+    }
+    finally {
+        spinner.style.display = 'none';
+    }
+}
+
+loadCard();
+
+
+
+
+
+
+
 
 
 loadCard();
