d3b70b1f54acabfcc13471751cfeae4ff51eead0
israt6898 <israt6898@gmail.com>
2024-03-06 15:43:01 +0600

    finish project

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const loadCard = async () => {
     const spinner = document.getElementById('spinner');
     spinner.style.display = 'block';
     try {
-        const res = await fetch(`  https://openapi.programming-hero.com/api/retro-forum/posts`);
+        const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
         const data = await res.json();
         const cards = data.posts;
         displayCard(cards);
@@ -22,7 +22,7 @@ const displayCard = cards => {
     cards.forEach(card => {
         const showCard = document.createElement('div');
         showCard.classList = `card card-side mb-[16px] bg-[#F3F3F5] shadow-xl`;
-        const indicatorStyle = card.isActive ? 'background-color: #00cc00' : ''
+        const indicatorStyle = card.isActive ? 'background-color: #00cc00' : 'background-color:red'
         showCard.innerHTML = `
         <figure>
             <div class="indicator">
@@ -61,7 +61,7 @@ const displayCard = cards => {
             <h1>${card.posted_time} Min</h1>
           </div>
 
-          <button onclick="displayCardTitle('${card.title}')"><i class="fa-regular fa-envelope text-center w-[40px] h-[40px] rounded-[50px] p-[8px] bg-[#10B981]" aria-hidden="true"></i></button>
+          <button onclick="displayCardTitle('${card.title}, ${card.view_count}')"><i class="fa-regular fa-envelope text-center w-[40px] h-[40px] rounded-[50px] p-[8px] bg-[#10B981]" aria-hidden="true"></i></button>
 
         </div>
       </div>
@@ -71,22 +71,27 @@ const displayCard = cards => {
 
 }
 
-const displayCardTitle = (title) => {
+const displayCardTitle = (title, view_count ) => {
     const sidebar = document.getElementById('sidebar');
     const titleCountElement = document.getElementById('titleCount');
 
-    const titleElement = document.createElement('div');
+    const titleElement = document.createElement('h1');
+    const p = document.createElement('p');
     titleCount++;
     titleElement.innerHTML = ` ${title}`;
+    //p.innerText = `${view_count}`;
+    
     sidebar.appendChild(titleElement);
-    titleCountElement.innerHTML = `Mark as read: ${titleCount}`;
+    sidebar.appendChild(p);
+    
+    titleCountElement.innerHTML = ` ${titleCount}`;
 }
 
 const handleSearch = async () => {
     const spinner = document.getElementById('spinner');
     spinner.style.display = 'block';
 
-    try {
+  try{
         const categorySearch = document.getElementById('categorySearch').value.toLowerCase();
         const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
         const data = await res.json();
@@ -108,7 +113,55 @@ loadCard();
 
 
 
+const loadingCard =async () =>{
+  const res = await fetch ("https://openapi.programming-hero.com/api/retro-forum/latest-posts");
+  const data = await res.json();
+  console.log(data);
+  showLastestCard(data);
+
+}
+
+const showLastestCard = data =>{
+  const lastestCard = document.getElementById("latest-card");
+  data.forEach(element => {
+    const viewCard = document.createElement('div');
+
+   
+    viewCard.classList = `card card-compact w-[350px] bg-base-100 shadow-xl`;
+
+    viewCard.innerHTML= ` <figure><img src="${element.cover_image}" class="h-[180px] w-[300px] pt-[30px]" alt="Shoes" /></figure>
+  
+    <div class="card-body">
+
+      <div>
+
+        <p class="ml-[10px]"><i class="fa-solid fa-calendar-days"></i> : ${element.author.posted_date? element.author.posted_date:'no date show' }</p>
+
+      </div>
 
 
 
-loadCard();
+      <h2 class="text-lg  font-extrabold  ml-[10px]">${element.title}</h2>
+
+      <p class="text-[#12132D99]">${element.description}</p>
+
+     <div class="flex gap-[16px]">
+
+      <div class="">
+
+         <img src="${element.profile_image}" class="w-[44px] h-[44px] rounded-full border-slate-900 border-2    " alt="" srcset=""> 
+
+  </div>
+
+  <div>
+
+    <h1>${element.author.name}</h1>
+
+    <p>${element.author.designation?element.author.designation:'no designation define'}</p>`
+
+    lastestCard.appendChild(viewCard);
+    
+  });
+}
+
+ loadingCard();
